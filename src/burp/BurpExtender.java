075699b9089de9a0d46fedb63b17c63834b4dfd9
tangzhihe <18326672274@163.com>
2017-08-04 09:13:41 +0800

    commit code

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -4,26 +4,24 @@ import java.io.PrintWriter;
 
 
 
-public class BurpExtender implements IBurpExtender, IHttpListener{
-	private PrintWriter stdout;
-	public IBurpExtenderCallbacks icallbacks;
-	@Override
-	public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
-		this.stdout = new PrintWriter(callbacks.getStdout(),true);
-		callbacks.setExtensionName("Her0in");//设置扩展名称 "Her0in"
-		callbacks.registerHttpListener(this);
-	}
+public class BurpExtender implements IBurpExtender, IScannerListener{
+	public PrintWriter stdout;
+	public IExtensionHelpers helpers;
 
 	@Override
-	public void processHttpMessage(int toolFlag, boolean messageIsRequest,
-			IHttpRequestResponse messageInfo) {
-		IHttpService iHttpService = messageInfo.getHttpService();
-		
-		this.stdout.print(iHttpService.getHost());
-		this.stdout.print(iHttpService.getPort());
-		this.stdout.print(iHttpService.getProtocol());
-	} 
+	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+		this.stdout = new PrintWriter(callbacks.getStdout(),true);
+		this.helpers = callbacks.getHelpers();
+		callbacks.setExtensionName("Her0in");
+		callbacks.registerScannerListener(this);
+	}
 	
+	@Override
+	public void newScanIssue(IScanIssue issue) {
+		stdout.println("扫描到新的问题 :");
+		stdout.println("url => " + issue.getUrl());
+		stdout.println("详情 => " + issue.getIssueDetail());
+	}
 }
 
 
